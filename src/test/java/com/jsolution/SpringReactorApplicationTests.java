d10047797184f4931b632fe59bc66dd4f54d6b70
Jiliar Silgado <jiliar.silgado@gmail.com>
2022-09-23 14:54:44 -0500

    SpringApplication Test

--- src/test/java/com/jsolution/SpringReactorApplicationTests.java
+++ src/test/java/com/jsolution/SpringReactorApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootTest
-@EnableMongoRepositories
 class SpringReactorApplicationTests {
 
     @Test
